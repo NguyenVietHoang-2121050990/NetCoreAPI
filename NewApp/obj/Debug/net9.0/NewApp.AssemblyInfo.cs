5e1e00d18ab3fa96f0a1cce1a76f2cff699bab12
2121050990-NguyenVietHoang <hoangnv5203@gmail.com>
2025-01-10 22:43:44 +0700

    Update_code

--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e36535f6edc8c00b6bca304c82589e8e2c42fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec31a279a68aa5fff5a96cc242db3595cb54b59")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
