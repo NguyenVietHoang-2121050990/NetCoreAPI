9726d94889a9d5b7b51faf79fb0c7ffd283ffd21
2121050990-NguyenVietHoang <hoangnv5203@gmail.com>
2025-03-17 15:06:28 +0700

    UpdateDataBase

--- DemoMVC/Models/BillModel.cs
+++
@@ -1,6 +0,0 @@
-public class BillModel
-{
-    public int Quantity { get; set; }
-    public double Price { get; set; }
-    public double Total { get; set; }
-}
--- DemoMVC/Models/GradeModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoMVC.Models{
-    public class GradeModel
-    {
-    public double ScoreA { get; set; }
-    public double ScoreB { get; set; }
-    public double ScoreC { get; set; }
-    public double FinalScore { get; set; }
-    }
-}
