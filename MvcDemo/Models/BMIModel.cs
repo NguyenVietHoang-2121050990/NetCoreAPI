769e25a3e2f006d0e16f6e8e23e083eb216815a7
2121050990-NguyenVietHoang <hoangnv5203@gmail.com>
2025-04-02 23:12:21 +0700

    finalcode

---
+++ MvcDemo/Models/BMIModel.cs
@@ -0,0 +1,10 @@
+namespace DemoMVC.Models
+{
+    public class BMIModel
+    {
+        public double Weight { get; set; } 
+        public double Height { get; set; } 
+        public double BMI { get; set; } 
+        public string? Result { get; set; } 
+    }
+}
